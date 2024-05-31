f15b52d4d937202496660a616d185de2d3a3825c
Dmitry <kde98@mail.ru>
2024-05-31 18:44:23 +0300

    hw 24.1

--- course/serializers.py
+++ course/serializers.py
@@ -9,6 +9,7 @@ class CourseSerializer(serializers.ModelSerializer):
         model = Course
         fields = '__all__'
 
+
 class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
